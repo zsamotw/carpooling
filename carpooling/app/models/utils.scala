1a71d6b39ead64cdb8004a1e489851390770f691
zsaomt <donkemot@interia.pl>
2017-05-18 15:07:04 +0200

    20170518

---
+++ carpooling/app/models/utils.scala
@@ -0,0 +1,25 @@
+package models
+
+import scala.io.Source
+import play.api.libs.json._
+
+object Utils {
+
+  def searchGeoPoint(user: UserFormData) = {
+    val query = "http://nominatim.openstreetmap.org/search/" + user.street + "," + user.city + ",Poland?format=json&polygon=1&addressdetails=1&limit=1"
+    val res = Source.fromURL(query).mkString
+    val jsonRes = Json.parse(res)
+    val lat = (jsonRes \\ "lat").head.toString
+    val lon = (jsonRes \\ "lon").head.toString
+    (lat, lon)
+  }
+
+  def searchGeoPoint(kg: KindergartenFormData) = {
+    val query = "http://nominatim.openstreetmap.org/search/" + kg.street + "," + kg.city + ",Poland?format=json&polygon=1&addressdetails=1&limit=1"
+    val res = Source.fromURL(query).mkString
+    val jsonRes = Json.parse(res)
+    val lat = (jsonRes \\ "lat").head.toString
+    val lon = (jsonRes \\ "lon").head.toString
+    (lat, lon)
+  }
+}
