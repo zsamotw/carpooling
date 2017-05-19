89c9df79a73aca717e4470664109e61ec488bd62
zsaomt <donkemot@interia.pl>
2017-05-19 10:29:38 +0200

    20170519

--- carpooling/app/controllers/HomeController.scala
+++ carpooling/app/controllers/HomeController.scala
@@ -30,37 +30,46 @@ class HomeController @Inject()(val messagesApi: MessagesApi)  extends Controller
   }
 
   def userMenu() = Action { implicit request =>
-    Ok(views.html.user(userForm.form))
-    }
+      Ok(views.html.user(userForm.form))
+  }
 
   def addUser() = Action {implicit  request =>
-    val uForm = userForm.form.bindFromRequest.get
-    val lenlon = Utils.searchGeoPoint(uForm)
-    val user = User(uForm.email, uForm.password, uForm.name, uForm.surname, uForm.city, uForm.street, uForm.kindergarten, lenlon._1, lenlon._2)
+    val userFromForm = userForm.form.bindFromRequest.get
+    val latLon = Utils.searchGeoPoint(userFromForm)
+    val user = User(userFromForm.email, userFromForm.password, userFromForm.name, userFromForm.surname, userFromForm.city, userFromForm.street, userFromForm.kindergarten, latLon._1, latLon._2)
     Users.add(user)
     Ok(views.html.index("User " + user.name + " was added"))
   }
 
   def kindergartenMenu() = Action { implicit request =>
-    Ok(views.html.addkindergarten(KindergartenForm.form))
+    request.session.get("connected").map { login =>
+      Ok(views.html.addkindergarten(KindergartenForm.form))
+    }.getOrElse {
+      Ok(views.html.index("You have to login first"))
+    }
   }
 
   def addKindergarten() = Action{ implicit request =>
     val kgFromForm = KindergartenForm.form.bindFromRequest.get
-    val kg = Kindergarten(kgFromForm.name, kgFromForm.street, kgFromForm.num, kgFromForm.city, "len", "lon")
+    val latLon = Utils.searchGeoPoint(kgFromForm)
+    val kg = Kindergarten(kgFromForm.name, kgFromForm.street, kgFromForm.num, kgFromForm.city, latLon._1, latLon._2)
     Kindergartens.add(kg)
     Ok(views.html.index("Kindergarten " + kg.name + " was added"))
   }
 
   def findKindergarten() = Action { implicit request =>
-    Ok(views.html.findparentsfromkindergarten(KindergartenForm.form))
+    request.session.get("connected").map { login =>
+      Ok(views.html.findparentsfromkindergarten(KindergartenForm.form))
+    }.getOrElse {
+      Ok(views.html.index("You have to login first"))
+    }
   }
 
   def showUsersFromKindergarten = Action {implicit request =>
     try {
     val kgFromForm = KindergartenForm.form.bindFromRequest.get
-    val lenlon = Utils.searchGeoPoint(kgFromForm)
-    val kg = Kindergarten(kgFromForm.name, kgFromForm.street, kgFromForm.num, kgFromForm.city, lenlon._1, lenlon._2)
+    val latLon = Utils.searchGeoPoint(kgFromForm)
+    val kg = Kindergarten(kgFromForm.name, kgFromForm.street, kgFromForm.num, kgFromForm.city, latLon._1, latLon._2)
     val usersFrom = Users.findUsersFromKindergarten(kgFromForm.name)
       Ok(views.html.parents(kg, usersFrom))
     } catch {
--- carpooling/app/models/utils.scala
+++ carpooling/app/models/utils.scala
@@ -7,15 +7,15 @@ object Utils {
 
   def searchGeoPoint(user: UserFormData) = {
     val query = "http://nominatim.openstreetmap.org/search/" + user.street + "," + user.city + ",Poland?format=json&polygon=1&addressdetails=1&limit=1"
-    val res = Source.fromURL(query).mkString
-    val jsonRes = Json.parse(res)
-    val lat = (jsonRes \\ "lat").head.toString
-    val lon = (jsonRes \\ "lon").head.toString
-    (lat, lon)
+    parseLatLonFromQuery(query)
   }
 
   def searchGeoPoint(kg: KindergartenFormData) = {
     val query = "http://nominatim.openstreetmap.org/search/" + kg.street + "," + kg.city + ",Poland?format=json&polygon=1&addressdetails=1&limit=1"
+    parseLatLonFromQuery(query)
+  }
+
+  def parseLatLonFromQuery(query: String) = {
     val res = Source.fromURL(query).mkString
     val jsonRes = Json.parse(res)
     val lat = (jsonRes \\ "lat").head.toString
