9f4397503a23cb5ddf02eb7c0051f2f1f9dc86c6
zsamotw <donkemot@interia.pl>
2018-03-16 11:20:58 +0100

    map 0.5

---
+++ carpooling/app/assets/javascripts/map.js
@@ -0,0 +1,50 @@
+function addMapLayer(name) {
+	map = new OpenLayers.Map(name);
+	map.addLayer(new OpenLayers.Layer.OSM());
+	return map;
+}
+
+function setLonLat(lon, lat) {
+
+	var lonLat = new OpenLayers.LonLat(lon, lat)
+                 .transform(
+                 new OpenLayers.Projection("EPSG:4326"), // transform
+																						// from
+																						// WGS
+																						// 1984
+                 map.getProjectionObject() // to
+																			// Spherical
+																			// Mercator
+																			// Projection
+                );
+	return lonLat;
+	
+}
+
+function setCenterAndZoom(lon,lat, map) {
+	var lonLat = setLonLat(lon, lat);
+                 
+	var zoom=12;
+	map.setCenter (lonLat, zoom);
+}
+
+function addMarkersLayer(map) {
+	var markers = new OpenLayers.Layer.Markers("Markers");
+	map.addLayer(markers);
+	return markers;
+}
+
+function setMarker(lon, lat, markers, popupText) {
+	var lonLat = setLonLat(lon, lat); 
+                 
+    markers.addMarker(new OpenLayers.Marker(lonLat));
+                 	
+    var popup = new OpenLayers.Popup("popup", lonLat,
+                new OpenLayers.Size(200,20),
+                popupText,
+                 true);
+    popup.show;
+    map.addPopup(popup);
+    }
+
+
